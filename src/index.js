5c774c69c6c3fd268251787fdf344c27a4738a20
Ivan Ivchik <63555972+IvanIvchik@users.noreply.github.com>
2022-02-07 21:17:02 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+   let m = 0;
+    if (n < 0) {
+        m = -n;
+    } else m = n;
+    let str = m.toString();
+    let result = str.split('').reverse().join('');
+    return +result; 
 }
